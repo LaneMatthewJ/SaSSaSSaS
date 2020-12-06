124a7dff37fbf79101699ae13d540244b2effbce
Jean Merlet <merletjj@ornl.gov>
2020-12-06 18:09:18 -0500

    update color loader with seed

--- load_flat_ncolor_data.py
+++ load_flat_ncolor_data.py
@@ -3,6 +3,9 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.utils import shuffle
 
+# set seed
+np.random.seed(522)
+
 def kmeans_image_to_ncolors(img, n_colors, n_subpixels=1000):
     kmeans = KMeans(n_clusters=n_colors, random_state=522)
     img = img / 255
@@ -36,10 +39,12 @@ def load_data_and_convert(path, n_colors):
     val, labels_val = val
     # combine train and val
     train = np.concatenate((train, val), axis=0)
-    labels_tr = np.concatenate((labels_tr, labels_val))
+    labels_tr = np.concatenate((labels_tr, labels_val), axis=None)
     # flatten pixels only
     train_rgb = train.reshape(train.shape[0], 3, 6400)
     test_rgb = test.reshape(test.shape[0], 3, 6400)
+    train_flat = train.reshape(train.shape[0], 19200)
+    print(train_flat[0][:10])
     # move channels to last dim
     train_rgb = np.swapaxes(train_rgb, 1, 2)
     test_rgb = np.swapaxes(test_rgb, 1, 2)
@@ -49,6 +54,6 @@ def load_data_and_convert(path, n_colors):
     return train_flat_2colors, test_flat_2colors, labels_tr, labels_te
 
 # example use:
-#path = './data/shipsnet.json'
-#n_colors = 2
-#train, test, labels_tr, labels_te = load_data_and_convert(path, n_colors)
+path = './data/shipsnet.json'
+n_colors = 2
+train, test, labels_tr, labels_te = load_data_and_convert(path, n_colors)
